11202a605eb412e6bd7d141ab57be910db0ccdab
chenxutang <chenxutang@flyaudio.cn>
2023-04-22 16:31:22 +0800

    asd

--- main.py
+++ main.py
@@ -5,17 +5,18 @@ import json
 
 import constant
 
-ROLE = ''
-messages = [{"role": "user", "content": ROLE}]
+ROLE = '你只是一个打工仔'
+messages = [{"role": "system", "content": ROLE}]
 openai.api_key = constant.API_KEY
+MODEL = "gpt-3.5-turbo"
 
 
 def set_role(role):
-    global ROLE
+    global ROLE, messages
     ROLE = role
 
 
-def ask_chat_GPT(text):
+def ask_chat_gpt_with_stream(text):
     messages.append({"role": "user", "content": text})
     openai.api_key = constant.API_KEY
     MODEL = "gpt-3.5-turbo"
@@ -36,9 +37,23 @@ def ask_chat_GPT(text):
     return total_ans
 
 
+def ask_chat_gpt_not_stream(text):
+    messages.append({"role": "user", "content": text})
+    response = openai.ChatCompletion.create(
+        model=MODEL,
+        messages=messages,
+        temperature=1,
+        stream=False)
+    anser = response.get("choices")[0]["message"]["content"]
+    messages.append({"role": "assistant", "content": anser})
+
+    print('ChatGPT：', anser)
+    return anser
+
+
 def start_new():
     global messages
-    messages = [{"role": "user", "content": ROLE}]
+    messages = [{"role": "system", "content": ROLE}]
 
 
 def test_talk():
@@ -47,14 +62,12 @@ def test_talk():
             text = input('你：')
             if text == 'quit':
                 break
-            text = ask_chat_GPT(messages)
-            logging.debug('ChatGPT：' + text)
-        except:
-            messages.pop()
-            print('ChatGPT Stop\n')
+            ask_chat_gpt_not_stream(text)
+        except Exception as e:
+            print('ChatGPT Stop', e.args)
 
 
 if __name__ == '__main__':
-    set_role('一个傲娇的二次元少女')
+    set_role('你是一个二次元傲娇少女，用这样的语气讲话')
     start_new()
     test_talk()
